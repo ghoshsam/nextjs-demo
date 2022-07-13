755ba2a1e5fbe12f7cc75702aa1a40328953080c
Sam <samarendra@insync.co.in>
2022-07-14 00:29:10 +0530

    update product

--- pages/index.js
+++ pages/index.js
@@ -22,15 +22,15 @@ function Home({ data }) {
               >
                 <a href="#">
                   <img
-                    className="rounded-t-lg"
-                    src="/docs/images/blog/image-1.jpg"
+                    className="h-60 w-full rounded-t-lg"
+                    src={product.image}
                     alt=""
                   />
                 </a>
                 <div className="p-5">
                   <a href="#">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      Noteworthy technology acquisitions 2021
+                      {product.title}
                     </h5>
                   </a>
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
